1064beed85c15960385ea87fb24cd71c77958247
twiens <wiens.thomas@gmail.com>
2016-09-09 15:36:01 +0200

    Introduced SkipPlusGraph class.

--- Skip_Graph/src/Main.java
+++ Skip_Graph/src/Main.java
@@ -1,33 +1,27 @@
-import java.util.ArrayList;
-import java.util.SortedSet;
-import java.util.TreeSet;
-
 /**
  * Created by twiens on 24.08.16.
  */
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        //for (int i = 0; i < Math.pow(2, 2); i++) {
-        //    Node tempNode = new Node();
-        //}
+        SkipPlusGraph graph = new SkipPlusGraph(3);
 
-        String[] nodesIds = new String[]{
-            "000",
-            "001",
-            "010",
-            "011",
-            "101",
-            "110",
-            "111",
-        };
+        Node n0 = new Node("000");
+        Node n1 = new Node("001");
+        Node n2 = new Node("010");
+        Node n3 = new Node("011");
+        Node n4 = new Node("100");
+        Node n5 = new Node("101");
+        Node n6 = new Node("110");
+        Node n7 = new Node("111");
 
-        int n = nodesIds.length;
-
-        ArrayList<Node> nodes = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            nodes.add(new Node(nodesIds[i]));
-        }
+        graph.join(n0);
+        graph.join(n1);
+        graph.join(n2);
+        graph.join(n3);
+        graph.join(n4);
+        graph.join(n5);
+        graph.join(n6);
+        graph.join(n7);
 
         //region alter schwacher Graph
 /*        //nodes[2].send(nodes[7]);
@@ -57,182 +51,23 @@ public class Main {
         //nodes[7].send(nodes[8]); */
         //endregion
 
-        for (int i = 0; i < n-1; i++) {
-            nodes.get(i).send(nodes.get(i+1));
-        }
-        nodes.get(0).send(nodes.get(n-1));
-
-        for (int i = 0; i < n; i++) {
-            nodes.get(i).start();
-        }
-
         // 3 sekunden laufen lassen
         Thread.sleep(3000);
 
-        Node newNode = new Node("100");
-        newNode.start();
-        nodes.get(6).send(newNode);
-        nodes.add(newNode);
-        n = nodes.size();
-
-        // 3 sekunden laufen lassen
-        Thread.sleep(3000);
-
-        for (int i = 0; i < n; i++) {
-            nodes.get(i).stopSubject();
-        }
-
-        for (int i = 0; i < n; i++) {
-            nodes.get(i).printNeighbourhood();
-        }
-
+        System.out.println("");
+        graph.printNeighbourHoodForAllLevels();
+        System.out.println("");
         System.out.println("################ Ultimativer Skip+-Graph Korrektheitstest ################");
-        System.out.println("Ergebnis = " + testSkipGraph(nodes));
-    }
+        boolean result = graph.testSkipPlusGraph();
+        System.out.println("################ Ergebnis = " + result + " ################");
 
-    private static boolean testSkipGraph(ArrayList<Node> nodes) {
-        TreeSet<Node> treeSet = new TreeSet<>();
-        for (Node node : nodes) {
-            treeSet.add(node);
-        }
-
-        for (Node node : nodes) {
-            // hole alle Nachfolger vom aktuellen Knoten
-            SortedSet<Node> successors = treeSet.tailSet(node, false);
-
-            // hole alle Vorgägner vom aktuellen Knoten
-            SortedSet<Node> predecessors = treeSet.headSet(node, false).descendingSet();    // invertiere die Menge, damit nächster Vorgänger
-                                                                                            // vorne ist
-
-            Range[] ranges = new Range[Node.NUMBER_OF_BITS];
-            for (int i = 0; i < Node.NUMBER_OF_BITS; i++) {
-                ranges[i] = new Range();
-
-                // Nachfolger bestimmen
-                boolean foundSuccessor0 = false;
-                boolean foundSuccessor1 = false;
-                Node successor1 = Node.maxNode;
-                Node successor0 = Node.maxNode;
-                for (Node successor : successors) {
-                    if (!foundSuccessor0 && Node.prefixMatch(i, node, successor, 0)) {
-                        successor0 = successor;
-                        foundSuccessor0 = true;
-
-                        if (node.successor[i].getNodeZero() != successor) {
-                            System.out.println("Successor 0 is wrong");
-                            System.out.println("Level: " + i + " Node: " + node.getID());
-                            System.out.println("Successor: " + node.successor[i].getNodeZero().getID() + " BUT should be " + successor.getID());
-                            return false;
-                        }
-                    }
-
-                    if (!foundSuccessor1 && Node.prefixMatch(i, node, successor, 1)) {
-                        successor1 = successor;
-                        foundSuccessor1 = true;
-                        if (node.successor[i].getNodeOne() != successor) {
-                            System.out.println("Successor 1 is wrong");
-                            System.out.println("Level: " + i + " Node: " + node.getID());
-                            if (node.successor[i].getNodeOne() != null) {
-                                System.out.println("Successor: " + node.successor[i].getNodeOne().getID() + " BUT should be " + successor.getID());
-                            } else {
-                                System.out.println("Successor: is null BUT should be " + successor.getID());
-                            }
-
-                            return false;
-                        }
-                    }
-                }
-
-                if (successor0.equals(Node.maxNode) && !successor1.equals(Node.maxNode)) {
-                    ranges[i].setEnd(successor1);
-                } else if (!successor0.equals(Node.maxNode) && successor1.equals(Node.maxNode)) {
-                    ranges[i].setEnd(successor0);
-                } else if (successor0.equals(Node.maxNode) && successor1.equals(Node.maxNode)) {
-                    ranges[i].setEnd(successor0);
-                } else if (successor0.isGreaterThan(successor1)) {
-                    ranges[i].setEnd(successor0);
-                } else if (successor1.isGreaterThan(successor0)) {
-                    ranges[i].setEnd(successor1);
-                }
-
-                // Vorgänger bestimmen
-                boolean foundPredecessor0 = false;
-                boolean foundPredecessor1 = false;
-                Node predecessor0 = Node.minNode;
-                Node predecessor1 = Node.minNode;
-                for (Node predecessor : predecessors) {
-                    if (!foundPredecessor0 && Node.prefixMatch(i, node, predecessor, 0)) {
-                        foundPredecessor0 = true;
-                        if (node.predecessor[i].getNodeZero() != predecessor) {
-                            System.out.println("Predecessor 0 is wrong");
-                            System.out.println("Level: " + i + " Node: " + node.getID());
-                            if (node.predecessor[i].getNodeZero() != null) {
-                                System.out.println("Predecessor: " + node.predecessor[i].getNodeZero().getID() + " BUT should be " + predecessor.getID());
-                            } else {
-                                System.out.println("Predecessor: is null BUT should be " + predecessor.getID());
-                            }
-
-                            return false;
-                        }
-
-                        predecessor0 = predecessor;
-                    }
-
-                    if (!foundPredecessor1 && Node.prefixMatch(i, node, predecessor, 1)) {
-                        foundPredecessor1 = true;
-                        if (node.predecessor[i].getNodeOne() != predecessor) {
-                            System.out.println("Predecessor 1 is wrong");
-                            return false;
-                        }
-
-                        predecessor1 = predecessor;
-                    }
-                }
-
-                if (predecessor0.equals(Node.minNode) && !predecessor1.equals(Node.minNode)) {
-                    ranges[i].setBegin(predecessor1);
-                } else if (!predecessor0.equals(Node.minNode) && predecessor1.equals(Node.minNode)) {
-                    ranges[i].setBegin(predecessor0);
-                } else if (predecessor0.equals(Node.minNode) && predecessor1.equals(Node.minNode)) {
-                    ranges[i].setBegin(predecessor0);
-                } else if (predecessor0.isLessThan(predecessor1)) {
-                    ranges[i].setBegin(predecessor0);
-                } else if (predecessor1.isLessThan(predecessor0)) {
-                    ranges[i].setBegin(predecessor1);
-                }
-
-                // vergleiche Ranges
-                if (node.range[i].getBegin() != ranges[i].getBegin()) {
-                    System.out.println("Range begin is wrong");
-                    System.out.println("Level: " + i);
-                    System.out.println("Node: " + node.getID() + " " + node.range[i] + " BUT should be " + ranges[i]);
-                    return false;
-                }
-
-                if (node.range[i].getEnd() != ranges[i].getEnd()) {
-                    System.out.println("Range end is wrong");
-                    System.out.println("Level: " + i);
-                    System.out.println("Node: " + node.getID() + " " + node.range[i] + " BUT should be " + ranges[i]);
-                    return false;
-                }
-
-                // vergleiche Nachbarschaften
-                for (Node n : nodes) {
-                    if (!n.equals(node) && node.range[i].isNodeInsideRange(n) && node.prefixMatch(i, n)) {
-                        if (!node.neighbours[i].contains(n)) {
-                            System.out.println("Neighbourhood is wrong");
-                            System.out.println("Level: " + i);
-                            System.out.println("Node: " + node.getID());
-                            System.out.println("Neighbourhoud");
-                            node.printNeighbourhood(i);
-                            System.out.println(" BUT should contain " + n.getID());
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-
-        return true;
+        graph.leave(n0);
+        graph.leave(n1);
+        graph.leave(n2);
+        graph.leave(n3);
+        graph.leave(n4);
+        graph.leave(n5);
+        graph.leave(n6);
+        graph.leave(n7);
     }
 }
